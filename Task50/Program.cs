f94c0968860c6a8ba4d9c737fbefbc0e1a4f8ea2
Pavel Arvachev <arvachev@gmail.com>
2022-09-10 17:13:19 +0300

    Add all task

---
+++ Task50/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая проверяет наличие указанного значения элемента в двумерном массиве или же указание, что такого элемента нет.
+Console.WriteLine("Задайте количество строк");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов");
+int n = int.Parse(Console.ReadLine());
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matr [i,j] = new Random().Next(0,101);
+        }
+    }
+}
+void PrintArray(int[,] matr)
+
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < n; j++)
+        {            
+            Console.Write($"{matr[i,j]} ");
+        }
+    }           
+}
+int [,] matr = new int[m,n];
+FillArray(matr);
+PrintArray(matr);
+Console.WriteLine();
+Console.WriteLine("Задайте число");
+int a = int.Parse(Console.ReadLine());
+int b = 0;
+for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (matr[i,j] == a)
+            {
+                b = 1;                           
+            }
+              
+        }
+    }
+if (b == 1)
+{
+    Console.WriteLine("Такое число есть в массиве");
+}
+else 
+{
+    Console.WriteLine("Такого числа нет в массиве");
+}   
